68f3cf0e9abe14c4576eb0605fb1330a391592fa
KingMaj0r <abdurrahman@barakat.email>
2024-08-08 18:52:15 +0700

    Moved all routing related stuff to pulseframe/routing.

--- src/InternalRoutes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use PulseFrame\Facades\Route;
-use PulseFrame\Http\Controllers\MaintenanceController;
-
-Route::get('/activate/{uuid}', [MaintenanceController::class, 'index'])
-->where('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
