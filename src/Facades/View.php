8d245290070500c131875d668e094d4bff56321e
KingMaj0r <abdurrahman@barakat.email>
2024-08-12 11:44:23 +0700

    Moved twig to PulseFrame/support-twig.

--- src/Facades/View.php
+++ src/Facades/View.php
@@ -5,10 +5,8 @@ namespace PulseFrame\Facades;
 use PulseFrame\Facades\Config;
 use PulseFrame\Facades\Env;
 use PulseFrame\Http\Handlers\RouteHandler;
-use Symfony\Component\HttpFoundation\Response;
-use Twig\Loader\FilesystemLoader;
-use Twig\Environment;
-use Exception;
+use PulseFrame\Facades\Response;
+use PulseFrame\Support\Engine;
 
 /**
  * Class View
@@ -16,12 +14,12 @@ use Exception;
  * @category facades
  * @name View
  * 
- * This class handles the rendering of Twig templates. It initializes Twig, manages environment configurations, 
+ * This class handles the rendering of View templates. It initializes the View engine, manages environment configurations, 
  * and integrates with the Vite asset builder for both development and production environments.
  */
 class View
 {
-  public static $twig;
+  public static $view;
   private $manifest;
 
   /**
@@ -29,7 +27,7 @@ class View
    *
    * @category facades
    * 
-   * This constructor checks if environment variables are loaded and initializes the Twig environment 
+   * This constructor checks if environment variables are loaded and initializes the View environment 
    * if it hasn't been initialized yet.
    * 
    * Example usage:
@@ -37,8 +35,11 @@ class View
    */
   public function __construct()
   {
-    if (!self::$twig) {
-      self::initialize();
+    if (!self::$view) {
+      self::$view = new Engine([
+        __DIR__ . '/../views/',
+        Config::get('view')['twig']['path'],
+      ]);
     }
 
     $manifestPath = ROOT_DIR . '/public/assets/.vite/manifest.json';
@@ -48,34 +49,15 @@ class View
   }
 
   /**
-   * Initialize the Twig environment.
+   * Render a View template.
    *
    * @category facades
    * 
-   * This static function sets up the Twig environment by specifying the template directory and enabling debug mode. 
-   * It also adds a debug extension to Twig.
-   * 
-   * Example usage:
-   * View::initialize();
-   */
-  private static function initialize()
-  {
-    $loader = new FilesystemLoader(Config::get('view')['twig']['path']);
-
-    self::$twig = new Environment($loader);
-    self::$twig->addExtension(new \Twig\Extension\DebugExtension());
-  }
-
-  /**
-   * Render a Twig template.
-   *
-   * @category facades
-   * 
-   * @param string $template The name of the Twig template.
+   * @param string $template The name of the View template.
    * @param array $data The data to pass to the template (optional).
    * @return string The rendered HTML content.
    *
-   * This static function renders a Twig template with the provided data. It checks if the Vite development 
+   * This static function renders a View template with the provided data. It checks if the Vite development 
    * server is running and renders appropriate error messages if required assets are missing.
    * 
    * Example usage:
@@ -85,11 +67,12 @@ class View
   {
     $manifest = (new self())->manifest;
 
-    if (!self::$twig) {
-      self::initialize();
+    if (!self::$view) {
+      new self();
     }
 
     $hotFile = $_ENV['storage_path'] . '/hot';
+
     $isDev = file_exists($hotFile);
 
     $data['isDev'] = $isDev;
@@ -103,13 +86,13 @@ class View
       if (Env::get("app.settings.debug")) {
         $message .= "<br>Hint: Make sure the Vite dev server is running.<br>If this is a mistake, delete the <code>storage/hot</code> file.";
       }
-      return self::$twig->render('error.twig', ['status' => "500", 'message' => $message]);
+      return self::$view->render('error.twig', ['status' => Response::HTTP_INTERNAL_SERVER_ERROR, 'message' => $message]);
     } elseif (!$isDev && (!isset($manifest) || empty($manifest))) {
       $message = "Manifest file not found";
       if (Env::get("app.settings.debug")) {
         $message .= "<br>Hint: Make sure you have built the project.<br>Not sure? Run <code>npx vite build</code>.";
       }
-      return self::$twig->render('error.twig', ['status' => "500", 'message' => $message]);
+      return self::$view->render('error.twig', ['status' => Response::HTTP_INTERNAL_SERVER_ERROR, 'message' => $message]);
     } else {
       $view = new self();
       $response = $view->renderView($template, $data);
@@ -138,6 +121,15 @@ class View
     return $headers && strpos($headers[0], '200') !== false;
   }
 
+  public static function assetUrl(string $path): string
+  {
+    if (env('IS_DEV') == 'true' || env('DEBUG', false)) {
+      return '/assets/' . ltrim($path, '/');
+    }
+
+    return 'http://0.0.0.0/assets/' . ltrim($path, '/');
+  }
+
   /**
    * Render a view with assets.
    *
@@ -147,7 +139,7 @@ class View
    * @param array $data The data to pass to the template (optional).
    * @return Response The HTTP response.
    *
-   * This function renders a Twig template and resolves Vite assets.
+   * This function renders a View template and resolves Vite assets.
    * 
    * Example usage:
    * $response = $view->renderView('template.vue', ['key' => 'value']);
@@ -166,28 +158,34 @@ class View
         'routes' => RouteHandler::$routeNames
       ], $data);
 
-      if (!$data['isDev']) {
-        try {
-          $data['assets'] = $this->resolveAssets(Config::get('app', 'entry'));
-        } catch (Exception $e) {
-          return new Response(
-            self::$twig->render('error.twig', [
-              'status' => 500,
-              'message' => $e->getMessage(),
-            ]),
-            Response::HTTP_INTERNAL_SERVER_ERROR
-          );
-        }
-      }
-
       if (isset($_SESSION['password'])) {
         unset($_SESSION['password']);
       }
 
       $template = 'base.twig';
+
+      try {
+        $assets = $this->resolveAssets(Config::get('app', 'entry'));
+
+        self::$view->addFunction(
+          'assetUrl',
+          function ($path) {
+            return $this->assetUrl($path);
+          }
+        );
+
+        $assetsTemplate = self::$view->render('assets.twig', ['assets' => $assets]);
+
+        $data['assets'] = $assetsTemplate;
+      } catch (\Exception $e) {
+        return self::$view->render('error.twig', [
+          'status' => Response::HTTP_INTERNAL_SERVER_ERROR,
+          'message' => $e->getMessage(),
+        ]);
+      }
     }
 
-    return new Response(self::$twig->render($template, $data));
+    return new Response(self::$view->render($template, $data));
   }
 
   /**
